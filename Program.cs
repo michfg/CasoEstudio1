5a82d781d986a6cccef011426637d3e967048504
michfg <michellefranciscoguzman@gmail,com>
2020-06-07 15:40:53 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CasoEstudio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
