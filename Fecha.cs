0ea2466c567f79fafe80fdf16b93b9f429290aad
michfg <michellefranciscoguzman@gmail,com>
2020-06-07 16:03:51 -0500

    producto

---
+++ Fecha.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CasoEstudio1
+{
+    class Fecha
+    {
+        public ushort Anio { get; set; }
+        public byte Mes { get; set; }
+        public byte Dia { get; set; }
+
+    }
+}
---
+++ Producto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CasoEstudio1
+{
+    class Producto
+    {
+        private Fecha FechaCaducidad ;
+        private int NumeroLote;
+        private String PaisOrigen;
+        private Fecha FechaEnvasado;
+    }
+}
